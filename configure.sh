d7b3a50005ddb9b476d29201df2543b2be8487ba
shuiyuechangchun <48248824+shuiyuechangchun@users.noreply.github.com>
2026-06-12 10:39:55 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 # Modify default system settings
 
 # 修改默认IP为192.168.123.1
-sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate 
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -11,8 +11,8 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 
 # passwall
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+# echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
+# echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
